2ee978df99cac7f3e7ebfaf9c1cd5fd740f7fe49
Artem Bilko <art.bilko14@gmail.com>
2025-05-25 19:29:36 +0300

    Rename folder

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App/App.jsx';
 import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from './store/store.js';
+import { store, persistor } from './redux/store.js';
 import { Provider } from 'react-redux';
 
 createRoot(document.getElementById('root')).render(
